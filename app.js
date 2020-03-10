4a5b90ecac9d7e96ffdbe00436987aad0765e9d7
Le Trong Thang <thangtl@hitachiconsulting.com>
2020-03-10 14:00:50 +0700

    Add welcom message

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ var express = require('express');
 var auth = require('./auth.js')
 var app = express();
 app.get('/', function (req, res) {
+  console.log(req.query.secret);
+  if(req.query.secret===undefined){
+    res.send('Cac chau oiiiii!');
+  }
   res.send(auth.getCode(req.query.secret));
 });
 
-var port=process.env.PORT;
+var port=process.env.PORT || 3000;
 app.listen(port, function () {
   console.log(`Example app listening on port ${port} !`);
 });
