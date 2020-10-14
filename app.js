afe42245c95402a17d123a2ac46dbe579676ca24
Thang <thang@Thangs-MacBook-Pro.local>
2020-10-14 20:14:55 +0700

    update code

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var auth = require('./auth.js')
 var app = express();
+
+app.use('/css',express.static(__dirname + '/public/css'));
+app.set('view engine','ejs');
+
+
 app.get('/', function (req, res) {
   console.log(req.query.secret);
-  if(req.query.secret===undefined){
-    res.send('Cac chau oiiiii!');
+  if(!req.query.secret){
+    res.render('index');
+  } else{
+    let code = auth.getCode(req.query.secret);
+    res.render('result',{data: {code: code}});
   }
-  res.send(auth.getCode(req.query.secret));
 });
 
 var port=process.env.PORT || 3000;
